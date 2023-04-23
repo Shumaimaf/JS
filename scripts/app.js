a8a3209900617776ba6df316f99386182378ce81
shumaimaf <shumaimaf@gmail.com>
2023-04-24 03:16:28 +0500

    assignment

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,42 @@
-var zakat_Percentage = 0.025;
+// let userAge = +prompt("Enter your age");
 
-var userInput = prompt("Enter your total wealth for Zakat calculation: ");
+// if (userAge >= 18) {
+//     alert("You are eligible for NIC");
+//  }else{
+//         alert("You are not eligible for NIC");
+//     }
+  
+    // multiple if
+//  let user = ("wania");
+//  let userRole = prompt("type your role")
 
-// userInput = parseInt(userInput);
-console.log(typeof Number(userInput));
+//  if (user == "moosa") {
+//     // alert("welcome sir");
+//     if(userRole == 'teacher'){
+//         alert('welcome sir')
+//     }
+//     else{
+//         alert('welcome beta')
+//     }
+//  }
+//  else{
+//         // alert("welcome students");
+//         alert('welcome beta');
+//      }
+ // single if
+// let user = prompt("moosa");
 
-var result = zakat_Percentage * userInput;
+// if (user == "moosa") {
+//    alert("welcome sir");
+// }
+//  else{
+//         alert("welcome students");
+//      }
+  
+// let user =prompt("type your name");
+//  let userRole = prompt("type your role")
 
-alert("Your Zakat value is " + result); 
-
+// if (user == "moosa" || user == "moosa" && userRole == "teacher"){
+//     alert("hello")
+// } else{
+//    alert('welcome beta'
