1f09848d78c8453698e05abe47f1517a563b9f29
shumaimaf <shumaimaf@gmail.com>
2023-04-25 20:51:53 +0500

    practise

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,99 @@
+// web programming language,client side programming language,writes head n body body tag.browser intrepret,
+// lightweight language,supports every browser that is called cross browser,powerful language,case sensative language,
+// ignores spaces
+
+       //    ALERT FUNCTION  alert means display,pop up,message to user.prints text, do calculation
+
+    // window.alert formal type
+// alert(3+2);
+// alert("hello world"); text string or string
+// alert('hello world'); ignores spaces except im text string
+// alert('2+1');
+// alert("2+1"); 
+   
+        //  document.write() fn prints text on browser,must end with semi colon and inside quotation
+
+// document.write("hello");
+// document.write('2+1');
+// document.write(2+1);
+
+        // VARIABLE
+        
+// var myname='shumaima';
+// var myname = ('shumaima');
+// var num = '2';
+// var num = 2+1;
+// var num = "2+1";
+// alert(num);
+  
+        // VARIABLES FOR STRING AND NUMB
+
+ 
+// var myname = 'shumaima';
+// myname = "khan";
+// alert(myname);
+
+// var firstname = "shumaima";
+// var lastname = "khan";
+// alert(firstname + lastname);
+
+// var age = "22";
+// var height = "5'7";
+// alert("Shumaima" + age + height);
+
+// var numb = "2+1";
+// var numb = 2+1;
+// alert(numb);
+
+// var my$ = "230"
+// alert(my$);
+
+    //   MATH EXPRESSION (operators)
+
+// var popularNum = '2+2';
+// var popularNum = 2+2;
+// var popularNum = 2-2;   
+// var popularNum = 2*4 ;
+// var popularNum = 9/3;
+// var popularNum = 10 % 3; modulus operator gives remainder
+// alert(popularNum)
+
+    //  Unfamiliar operators
+
+// var num = 1;
+// var newNum = num++ ; 
+// var newNum = ++num ; incremented
+// var newNum = num-- ;
+// var newNum = --num ; decremented
+// console.log(newNum);    \
+
+        // ELIMINATIING AMBIGUITY
+
+// var totalCost = 1+3*4;
+// var totalCost = 1+(3*4);
+// var totalCost = (2*2) + (3*4);
+// var totalCost = 2*2 + 3*4;
+// console.log(totalCost);
+
+    // Concatenating text strings
+    
+// var message = 'thanks';
+// alert(message); 
+
+// var userName = "Shumaima";
+// alert("Thanks, " + userName + " !");
+   
+    //   Concatenation to var \
+
+// var message = 'Thanks';
+// var userName = "Shumaima";
+// var banger = "!";
+// var customMess = message + userName + banger;
+// alert(customMess);
+
+       
+
+
 // let userAge = +prompt("Enter your age");
 
 // if (userAge >= 18) {
