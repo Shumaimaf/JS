c4e6111e434c3ceb048e28857cabea57e233aefc
shumaimaf <shumaimaf@gmail.com>
2023-05-08 00:14:09 +0500

    practise

--- scripts/app.js
+++ scripts/app.js
@@ -136,6 +136,13 @@
 // var whatever = "hello";
 // tellTime()
 
+    //  TASK 1
+    
+    // function leapYear(year) {
+    //     return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+    //   }
+    //   var typeYear = prompt('enter year'); 
+    //     console.log(leapYear(typeYear));
 
 // function greetUser(greetings) {
 // alert(greetings);    
